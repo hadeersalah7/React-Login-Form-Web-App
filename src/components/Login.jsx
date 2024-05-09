0c63a7c9bcd2a5ee6bc00a24ee5699aa300951f1
Hadeer Salah <hadeersalah127@gmail.com>
2024-05-09 22:29:00 +0300

    updating the email & pass

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: ""
+  })
+  const handleFormSubmit = (identifir,e) =>{
+    e.preventDefault()
+  
+  }
+
+  const handleInputChange = (identifir, e) => {
+    setEnteredValues(prev => ({
+    ...prev,
+      [identifir]: e.target.value
+    }))
+  }
   return (
     <form>
       <h2>Login</h2>
@@ -6,12 +23,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(e) => handleInputChange("email",e)} value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(e) => handleInputChange("password", e)} value={enteredValues.password}/>
         </div>
       </div>
 
